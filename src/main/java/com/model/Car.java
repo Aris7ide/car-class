5c3e560ecf2d961ee3107da9eb16a15e10ab1722
Aris <aris7ide@gmail.com>
2026-09-08 22:47:22 +0200

    feat: add class Car with attributes

---
+++ src/main/java/com/model/Car.java
@@ -0,0 +1,25 @@
+package com.model;
+
+public class Car {
+
+    public static final String BRAND = "Mercedes";
+    private static String model = "";
+    private final int power;
+
+    public Car(int power) {
+        this.power = power;
+
+    }
+
+    public static String getModel() {
+        return model;
+    }
+
+    public static void setModel(String model) {
+        Car.model = model;
+    }
+
+    public int getPower() {
+        return power;
+    }
+}
